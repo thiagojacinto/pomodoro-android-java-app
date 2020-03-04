78493f251ad4db8a7b93af13bdefb039205fd310
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-03 22:51:42 -0300

    [new] timer model

---
+++ app/app/src/main/java/com/pomodoroTimer/model/PomodoroTimer.java
@@ -0,0 +1,50 @@
+package com.pomodoroTimer.model;
+
+import java.util.Random;
+
+public class PomodoroTimer {
+
+    // Attributes
+    private long id;
+    private String label, duration;
+    private String DEFAULT_25_MINUTES = "25";
+
+    // Constructor:
+    //
+    // (a) default 25-minute;
+    public PomodoroTimer(long id, String label) {
+        // Constant duration of 25 minutes
+
+        this.id = id;
+        this.label = label;
+        this.duration = DEFAULT_25_MINUTES;
+    }
+
+    // (b) customized duration;
+    public PomodoroTimer(long id, String label, String duration) {
+        this.id = id;
+        this.label = label;
+        this.duration = duration;
+    }
+
+    // (c) empty
+    public PomodoroTimer() {
+        this.id = new Random().nextLong()*10000L;
+        this.label = "Random Timer";
+        this.duration = DEFAULT_25_MINUTES;
+    }
+
+    // GETTERS
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
