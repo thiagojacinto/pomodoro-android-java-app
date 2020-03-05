28e093a58807c41d81f238bddf95d38a8af7ccfe
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-04 22:42:14 -0300

    [init] let's start from the begining

--- app/app/src/androidTest/java/com/litepomodoroapp/ExampleInstrumentedTest.java
+++ app/app/src/androidTest/java/com/litepomodoroapp/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.pomodoroTimer;
+package com.litepomodoroapp;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.pomodoroTimer", appContext.getPackageName());
+        assertEquals("com.litepomodoroapp", appContext.getPackageName());
     }
 }
